6a2f5e46e7ea63aad8535a241c641af665235505
Riluwan <89859640+Riluwan@users.noreply.github.com>
2024-05-14 20:31:54 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -6,9 +6,42 @@
 import pymongo, os
 from config import DB_URI, DB_NAME
 from pymongo.server_api import ServerApi
+'''
+# Assumed you have run 'pip install pymongo'
+import pymongo
+databaseName = 'filesharexbot'
+settings = {
+  'host': 'iad2-c18-0.mongo.objectrocket.com:52144,iad2-c18-2.mongo.objectrocket.com:52144,iad2-c18-1.mongo.objectrocket.com:52144',
+  'database': databaseName,
+  'username': 'YOUR_USERNAME',
+  'password': 'YOUR_PASSWORD',
+  'options': '?replicaSet=d6dfa76c799445c7bd9083f88862329b'
+}
+
+try:
+   conn = pymongo.MongoClient("mongodb://{username}:{password}@{host}/{database}{options}".format(**settings))
+   collectionNames = conn[databaseName].collection_names()
+   print("Connected")
+   print("Collection Names {}".format(collectionNames))
+   conn.close()
+except Exception as ex:
+   print("Error: {}".format(ex))
+   exit('Failed to connect, terminating.')
+
+print("Finished") # Done!
+
+'''
+databaseName = 'filesharexbot'
+settings = {
+  'host': 'iad2-c18-0.mongo.objectrocket.com:52144,iad2-c18-2.mongo.objectrocket.com:52144,iad2-c18-1.mongo.objectrocket.com:52144',
+  'database': databaseName,
+  'username': 'vyrxen123',
+  'password': 'vyrxen123',
+  'options': '?replicaSet=d6dfa76c799445c7bd9083f88862329b'
+}
 
 
-dbclient = pymongo.MongoClient(DB_URI,server_api=ServerApi('1'))
+dbclient = pymongo.MongoClient("mongodb://{username}:{password}@{host}/{database}{options}".format(**settings))
 database = dbclient[DB_NAME]
 
 
